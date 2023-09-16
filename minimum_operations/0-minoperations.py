85eff45b9aec52f287fbfe3a08265fe41462f8ef
lanaarafat <a.mutoni1@alustudent.com>
2023-09-16 21:19:32 +0200

    minimum op

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+"""
+Mmethod that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""
 
-"""
-method that calculates the fewest number of operations needed to result in exactly n H characters in the file
-"""
 
 def minOperations(n):
+
     if n <= 1:
         return 0
-
     for i in range(2, n + 1):
         if n % i == 0:
             return i + minOperations(n // i)
     return 0
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     print(minOperations(19170307))
     print(minOperations(972))
     print(minOperations(2147483640))
-
+    
