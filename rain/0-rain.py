b5cf62cb867b7b79ca625cc2950cf0dc73aa8bda
lanaarafat <a.mutoni1@alustudent.com>
2023-10-08 22:16:56 +0200

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,30 +3,22 @@ def rain(walls):
     if not walls:
         return 0
 
-    n = len(walls)
-    left_max = [0] * n
-    right_max = [0] * n
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
 
-    # Calculate the maximum height to the left of each wall
-    left_max[0] = walls[0]
-    for i in range(1, n):
-        left_max[i] = max(left_max[i - 1], walls[i])
+    while left < right:
+        if walls[left] <= walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
 
-    # Calculate the maximum height to the right of each wall
-    right_max[n - 1] = walls[n - 1]
-    for i in range(n - 2, -1, -1):
-        right_max[i] = max(right_max[i + 1], walls[i])
-
-    # Calculate the amount of water retained at each position
-    water_retained = 0
-    for i in range(n):
-        water_retained += min(left_max[i], right_max[i]) - walls[i]
-
-    return water_retained
-
-# Test cases
-walls1 = [0, 1, 0, 2, 0, 3, 0, 4]
-walls2 = [2, 0, 0, 4, 0, 0, 1, 0]
-
-print(rain(walls1))
-print(rain(walls2))
+    return water
